ad698fa46993d146d344547d933fa44c11c8e89a
Eloy Navarro Morales <enavarro@zeta40.aulas.gsyc.es>
2016-02-11 10:39:26 +0100

    Modificaciones profesor + opcion sin completar

--- calc.py
+++ calc.py
@@ -3,43 +3,38 @@ import sys
 # calculadora(Suma, op1, op2)
 
 
-Funcion = sys.argv[1]
-Op1 = float(sys.argv[2])
-Op2 = float(sys.argv[3])
+#Funcion = sys.argv[1]
+#Op1 = float(sys.argv[2])
+#Op2 = float(sys.argv[3])
+
+if len(sys.argv) != 4:
+    sys.exit ("introducir 3 argumentos")
+
+_, Funcion, Op1, Op2 = sys.argv
+
+try:
+    Op1 = float(Op1)
+    Op2 = float(Op2)
+except:
+    sys.exit ("Necesito numeros")
+    
+    
+#funciones = ["suma", "resta", ]
+
+#if operador in funciones:
+
+#else sys.exit("Operadores: suma, resta, divide, multiplica")
+
+
+
 if Funcion == "suma":
     print str(Op1), "+", str(Op2), "=", (Op1+Op2)
 if Funcion == "resta":
     print str(Op1), "-", str(Op2), "=", (Op1-Op2)
 if Funcion == "divide":
-    print str(Op1), "/", str(Op2), "=", (Op1/Op2)
+    try:
+        print str(Op1), "/", str(Op2), "=", (Op1/Op2)
+    except ZeroDivisionError:
+        print "Division por cero"
 if Funcion == "multiplica":
     print str(Op1), "*", str(Op2), "=", (Op1*Op2)
-
-
-
-#print "Arg0: ", sys.argv[0]
-#print "Number of arguments: ", len(sys.argv)
-#print "The arguments are: " , str(sys.argv)
-
-
-"""
-AuxFile = open ("/etc/passwd","r")  # Archivo -> auxiliar
-lineas = AuxFile.readlines()        # Auxiliar -> lista[linea, caracter]
-
-shells = {}             # Init diccionario
-for linea in lineas:    # Recorro cada linea leida del fichero
-    shells[linea.split(":")[0]] = linea.split(":")[-1][:-1] # Cargo el shell de
-                                                            # cada root en el
-                                                            # diccionario.
-print "Numero de shells: " + str(len(shells))   # Imprimo la cantidad de shells
-try:                                        
-    print "shell de root:", shells["root"]
-    print "shell de imaginario:", shells["imaginario"]
-#    print "usuario" + blabla... + es distinto de 
-#    print "usuario", blabla...,
-#    en cuanto a "instante de ejecucion"
-except: # Permite especificar el fallo concreto a capturar de los disponibles
-    print "Usuario no encontrado en el diccionario."
-
-AuxFile.close
-"""
